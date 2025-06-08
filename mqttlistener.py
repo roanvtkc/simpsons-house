1667cf36e850c14106eb5b18d06fa2ec05b6666b
roanvtkc <roanv@tkc.wa.edu.au>
2025-06-08 19:33:41 +0800

    mqttlistener.py

---
+++ mqttlistener.py
@@ -0,0 +1,17 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
+    client.subscribe("light")
+    client.subscribe("fan")
+    client.subscribe("door")
+
+def on_message(client, userdata, msg):
+    print(f"Topic: {msg.topic}, Message: {msg.payload.decode()}")
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("localhost", 1883, 60)
+client.loop_forever()
